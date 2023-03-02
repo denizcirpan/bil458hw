54fe7cddb9dded5168613a1752a7c5fefe199856
Deniz <21994730@mail.baskent.edu.tr>
2023-03-02 23:59:39 +0300

    DENIZ CIRPAN

---
+++ src/EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is Even");
+        else
+            System.out.println(num + " is Odd");
+	}
+
+}
